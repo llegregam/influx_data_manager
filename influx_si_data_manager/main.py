f043c4f41ea2cad875ad31019016fda2b92927e6
llegregam <legregam@insa-toulouse.fr>
2023-12-04 11:06:58 +0100

    Added zip file handling

--- influx_si_data_manager/main.py
+++ influx_si_data_manager/main.py
@@ -1,8 +1,10 @@
 import argparse
 import logging
 import sys
-from ast import literal_eval
+import zipfile
 from pathlib import Path
+
+import pandas as pd
 
 from utils.isocor2mtf import isocor2mtf
 from utils.physiofit2mtf import physiofit2mtf
@@ -40,40 +42,64 @@ def args_parse():
         "workflow4metabolomics.usegalaxy.org"
     )
 
+    influx_files = parser.add_argument_group(
+        "Influx input files",
+        "List of all the input files that influx_si can accept. Some are variable and change for each experiment "
+        "(.mflux or physiofit output, .miso or isocor output, .mmet, etc) and some are static for the same batch of "
+        "experiments (.netw, .linp, etc). Check out the influx user documentation for more information."
+    )
+
     # Get paths to data
-    parser.add_argument(
+    influx_files.add_argument(
         "-p", "--physiofit", type=str,
         help="Path to physiofit summary output file"
     )
-    parser.add_argument(
+    influx_files.add_argument(
         "-i", "--isocor", type=str,
         help="Path to isocor results output file"
     )
-    parser.add_argument(
-        "-m", "--misc", type=str,
-        help="Path to collection containing paths to the extra files for influx_si (given as list)"
+    influx_files.add_argument(
+        "-li", "--linp", type=str,
+        help="Path to .linp file containing the label input information"
+    )
+    influx_files.add_argument(
+        "-ne", "--netw", type=str,
+        help="Path to .netw file containing the list of biochemical reactions & label transitions"
+    )
+    influx_files.add_argument(
+        "-mm", "--mmet", type=str,
+        help="Path to .mmet file containing the stationary specie information"
+    )
+    influx_files.add_argument(
+        "-cn", "--cnstr", type=str,
+        help="Path to .cnstr file containing constraints on fluxes and specie conentrations"
+    )
+    influx_files.add_argument(
+        "-tv", "--tvar", type=str,
+        help="Path to .tvar file containing the types of variables"
+    )
+    influx_files.add_argument(
+        "-op", "--opt", type=str,
+        help="Path to .opt file containing extra options to pass to influx_si"
     )
 
     # Give output collection paths
     parser.add_argument(
-        "-z", "--zip", type=str,
+        "-z", "--zip", type="store_true",
         help="Output path for zip containing all the files for influx launch"
     )
+
     parser.add_argument(
         "-l", "--log", type=str,
         help="Output path for log"
     )
 
-    # # Add for galaxy launch
-    # parser.add_argument(
-    #     "-g", "--galaxy", action="store_true",
-    #     help="Add argument for galaxy launch (to get path from args)"
-    # )
 
     return parser
 
 
 def process(args):
+
     # initialize root
     if hasattr(args, "log"):
         _init_logger(str(Path(args.log)))
@@ -83,26 +109,16 @@ def process(args):
     # get logger
     _logger = logging.getLogger("root")
 
-    if hasattr(args, "galaxy"):
-        mflux_dfs = physiofit2mtf(
-            physiofit_res=args.physiofit
-        )
-        miso_dfs = isocor2mtf(
-            isocor_res=args.isocor
-        )
-        list_of_miscs = args.misc
-    else:
-        mflux_dfs = physiofit2mtf(
-            physiofit_res=r"C:\Users\legregam\PycharmProjects\influx_si_data_manager\test-data\physiofit2mtf"
-                          r"\summary.csv",
-        )
+    _logger.info("Run arguments:")
+    for key, val in vars(args).items():
+       _logger.info(f"{key} : {val}")
 
-        miso_dfs = isocor2mtf(
-            isocor_res=r"C:\Users\legregam\PycharmProjects\influx_si_data_manager\test-data\isocor2mtf"
-                       r"\Data_example_res.tsv",
-            sd=0.02
-        )
-        list_of_miscs = literal_eval(args.misc)
+    mflux_dfs = physiofit2mtf(
+        physiofit_res=args.physiofit
+    )
+    miso_dfs = isocor2mtf(
+        isocor_res=args.isocor
+    )
 
     mflux_names = [exp[0] for exp in mflux_dfs]
     miso_names = [exp[0] for exp in miso_dfs]
@@ -113,19 +129,35 @@ def process(args):
         _logger.error(msg)
         raise ValueError(msg)
 
-    mflux_path = Path("./mflux")
-    if not mflux_path.exists():
-        mflux_path.mkdir()
-    miso_path = Path('./miso')
-    if not miso_path.exists():
-        miso_path.mkdir()
-    for exp in mflux_dfs:
-        exp[1].to_csv(str(mflux_path / f"{exp[0]}.tsv"), sep="\t")
-    for exp in miso_dfs:
-        exp[1].to_csv(str(miso_path / f"{exp[0]}.tsv"), sep="\t")
+    _logger.info(f"Experiment Names:\n{mflux_names}")
+
+    non_var_files = [
+        "linp",
+        "netw",
+        "mmet",
+        "constr",
+        "tvar",
+        "opt"
+    ]
+    for mflux, miso in zip(mflux_dfs, miso_dfs):
+        with zipfile.ZipFile(f"{mflux[0]}.zip", "w", compression=zipfile.ZIP_DEFLATED) as output_zip:
+            with output_zip.open(f"{mflux[0]}.mflux", "w") as mflux_file:
+                mflux[1].to_csv(mflux_file, index=False, sep="\t")
+            with output_zip.open(f"{miso[0]}.miso", "w") as miso_file:
+                miso[1].to_csv(miso_file, index=False, sep="\t")
+            for nvf in non_var_files:
+                df = pd.read_csv(nvf, sep="\t")
+                with output_zip.open(f"{mflux[0]}.{nvf}", "w") as nvf_file:
+                    df.to_csv(nvf_file, index=False, sep="\t")
+
 
 
 def main():
     parser = args_parse()
     args = parser.parse_args()
-    process(args)
+    # process(args)
+    print(parser)
+    print(vars(args))
+
+if __name__ == "__main__":
+    main()
