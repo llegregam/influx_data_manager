c442ae092c3b31070dbc6f766ff45bb97eb4acb4
llegregam <legregam@insa-toulouse.fr>
2025-03-10 16:49:44 +0100

    refactored map_data func

--- influx_si_data_manager/utils/map_data.py
+++ influx_si_data_manager/utils/map_data.py
@@ -2,24 +2,24 @@ import pandas as pd
 
 CONNECTOR_NAME = "influx_si_data_connector"
 
-def map_data(mapping_file, data):
+def map_data(mapping_file, data, from_tool):
     """
     Replace cell values in dataset by following indications in mapping file
     :param mapping_file: Path to the mapping file
-    :param dataset: dataset to modify
+    :param data: dataset to modify
+    :param from_tool: tool name to filter mapping entries
     :return: Mapped data
     """
-
-    # Read the mapping file
+    # Read the mapping file and filter for relevant entries
     mapping = pd.read_csv(mapping_file, sep="\t")
-    mapping = mapping.loc[mapping["Connector"] == CONNECTOR_NAME, :]
-
-    for key in data:
-        current_tool_view = mapping.loc[mapping["From_tool"] == key]
-        for column in current_tool_view["Column"].unique():
-            current_column_view = current_tool_view.loc[current_tool_view["Column"] == column]
-            to_replace = current_column_view[["Replace", "By"]].set_index("Replace").to_dict()["By"]
-            data[key][column] = data[key][column].replace(to_replace)
+    filtered_mapping = mapping[(mapping["Connector"] == CONNECTOR_NAME) & 
+                              (mapping["From_tool"] == from_tool)]
+    
+    # Replace values in each column
+    for column in filtered_mapping["Column"].unique():
+        replacements = filtered_mapping[filtered_mapping["Column"] == column]
+        replace_dict = dict(zip(replacements["Replace"], replacements["By"]))
+        data[column] = data[column].replace(replace_dict)
     
     return data
             
@@ -28,5 +28,7 @@ def map_data(mapping_file, data):
 if __name__ == "__main__":
     
     isocor_data = pd.read_csv("../test_data/isocor_results.tabular", sep="\t")
+    print(isocor_data)
     datasets = {"isocor": isocor_data}
-    replaced = map_data(r"../test_data/mapping.txt", data=datasets)
+    replaced = map_data(r"../test_data/mapping.txt", data=isocor_data, from_tool="isocor")
+    print(replaced)
