817e7f7338ce88010dc6c4ed35254f6e6cd08807
llegregam <legregam@insa-toulouse.fr>
2025-04-30 11:20:27 +0200

    Added some logging

--- influx_si_data_manager/utils/map_data.py
+++ influx_si_data_manager/utils/map_data.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import logging
 
 CONNECTOR_NAME = "influx_si_data_connector"
+
+logger = logging.getLogger("root.influx_si_data_manager.utils.map_data")
 
 def map_data(mapping_file, data, from_tool):
     """
@@ -17,9 +20,16 @@ def map_data(mapping_file, data, from_tool):
     
     # Replace values in each column
     for column in filtered_mapping["Column"].unique():
+        print(f"Data to modify: {data}")
+        logger.debug(f"Replacing values in column: {column}")
+        logger.debug(f"Data to modify: {data}")
         replacements = filtered_mapping[filtered_mapping["Column"] == column]
         replace_dict = dict(zip(replacements["Replace"], replacements["By"]))
+        logger.debug(f"Replacement dictionary: {replace_dict}")
+        print(replace_dict)
+        logger.debug(f"Column values before replacement: {data[column]}")
         data[column] = data[column].replace(replace_dict)
+        logger.debug(f"Column values after replacement: {data[column]}")
     
     return data
             
@@ -27,8 +37,9 @@ def map_data(mapping_file, data, from_tool):
 
 if __name__ == "__main__":
     
-    isocor_data = pd.read_csv("../test_data/isocor_results.tabular", sep="\t")
-    print(isocor_data)
-    datasets = {"isocor": isocor_data}
-    replaced = map_data(r"../test_data/mapping.txt", data=isocor_data, from_tool="isocor")
+    #isocor_data = pd.read_csv("../test_data/isocor_results.tabular", sep="\t")
+    #print(isocor_data)
+    physiofit_data = pd.read_csv(r"/home/llegregam/Documents/tools_w4m/packages/influx_data_manager/influx_si_data_manager/test_data/summary.csv", sep=",")
+    print(physiofit_data)
+    replaced = map_data(r"/home/llegregam/Documents/tools_w4m/packages/influx_data_manager/influx_si_data_manager/test_data/mapping.txt", data=physiofit_data, from_tool="physiofit")
     print(replaced)
